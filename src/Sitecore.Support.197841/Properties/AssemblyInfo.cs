3c338cb87559d62b29992c25b025c054932e19a7
DK\psk <psk@sitecore.net>
2018-05-04 18:25:34 +0300

    Empty solution

---
+++ src/Sitecore.Support.197841/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.197841")]
+[assembly: AssemblyProduct("Sitecore.Support.197841")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Suppress DependencyResolutionException in SitecorePerRequestScopeModule. Prevent ObjectDisposedException in AutowiredPageHandlerFactory.")]
+
